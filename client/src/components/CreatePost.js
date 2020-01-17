a4cc61e5ac50b511cc37ac56cd6288ca0ba07bb1
Ian Kincaid <ikbusinessacct@gmail.com>
2020-01-17 10:01:51 -0500

    connections db name change and create post component made

---
+++ client/src/components/CreatePost.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import axios from 'axios'
+
+export default class CreatePost extends React.Component {
+
+
+  render() {
+      return(
+         <div>
+             <h1>Create Post Page</h1>
+         </div> 
+      )
+  }
+
+}
