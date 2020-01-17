49d695fc4cb13f53f2f583d8800f3189e0e283b7
Ian Kincaid <ikbusinessacct@gmail.com>
2020-01-17 13:22:58 -0500

    models get all, get one, create, update, delete all added

--- models/post.js
+++ models/post.js
@@ -1,57 +1,38 @@
-/* 
- * Place all functions, classes, and/or DB schemas here for a single 
- * model.
- */
+const mongoose = require('./connection.js')
+const Schema = mongoose.Schema;
 
-/* Step 1
- *
- * TODO: import mongoose connection
- * NOTE: skip this if you are not using mongoose
- *
- */
-//const mongoose = require('./connection.js')
+const PostSchema = new Schema({
+  title: String,
+  image: String,
+  description: String,
+}) 
 
-/* Step 1 alternative
- *
- * TODO: make a global variable to act as an in memory database. 
- * NOTE: doing this WILL NOT persist your data and you will loose
- * your data once you stop running your server.
- *
- */
-// global.sampleModel = [];
+const PostCollection = mongoose.model('Post', PostSchema)
 
-/* Step 2
- *
- * TODO: create model schema 
- * NOTE: skip this if you are not using mongoose
- *
- */
-//const SampleModelSchema = new mongoose.Schema({
-//  name: String
-//})
-
-/* Step 3
- *
- * TODO: create collection API
- * NOTE: skip this if you are not using mongoose
- *
- */
-//const SampleCollection = mongoose.model('Sample', SampleModelSchema)
-
-/* Step 4
- *
- * TODO: delete this it's just a sample
- *
- */
-function getHelloWorldString() {
-  return 'hello world'
+const getAllPost = () => {
+  return PostCollection.find()
 }
 
-/* Step 5
- *
- * TODO: export all functions from this file by adding their names as keys to this
- * object
- */
+const singlePost = (postId) => {
+  return PostCollection.findById(postId)
+}
+
+const addPost = (postId) => {
+  return PostCollection.create(postId)
+}
+
+const updatePost = (postId, updatePost) => {
+  return PostCollection.findByIdAndUpdate(postId, updatePost)
+}
+
+const deletePost = (postId) => {
+  return PostCollection.deleteOne({id_: postId})
+}
+
 module.exports = {
-  getHelloWorldString
+  getAllPost,
+  singlePost,
+  addPost,
+  updatePost,
+  deletePost
 }
