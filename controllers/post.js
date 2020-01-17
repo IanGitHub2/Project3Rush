476563707f11a303bc5bbd5fb8ae7bc4b3a8eb2a
Ian Kincaid <ikbusinessacct@gmail.com>
2020-01-17 14:05:41 -0500

    controllers get all, get one, create, update, delete added

--- controllers/post.js
+++ controllers/post.js
@@ -1,50 +1,43 @@
-/* Step 1 import express
- *
- */
 const express = require('express')
-
-/* Step 2
- *
- * Import the api files from the models
- *
- * TODO: change the file path to the models file you'll need to use.
- * TODO: rename this from `templateApi` to something more sensible (e.g:
- * `shopsAPI`)
- *
- * NOTE: You may need to import more than one API to create the 
- * controller you need.
- * 
- */
 const postApi = require('../models/post.js')
-
-/* Step 3 
- * 
- * Create a new router.
- *
- * the router will "contain" all the request handlers that you define in this file.
- * TODO: rename this from templateRouter to something that makes sense. (e.g:
- * `shopRouter`)
- */
 const postRouter = express.Router()
 
-/* Step 4
- * 
- * TODO: Put all request handlers here
- */
 
-/* Step 5
- *
- * TODO: delete this handler; it's just a sample
- */ 
-templateRouter.get('/', (req, res) => {
-  res.json(templateApi.getHelloWorldString())
+postRouter.get('/', (req, res) => {
+  postApi.getAllPost()
+    .then((allPost) => {
+      res.json(allPost)
+    })
 })
 
-/* Step 6
- *
- * Export the router from the file.
- *
- */
+postRouter.get('/:postId', (req, res) => {
+  postApi.singlePost()
+    .then((post) => {
+      res.json(post)
+    })
+})
+
+postRouter.post('/', (req, res) => {
+  postApi.addPost(req/body)
+    .then((post) => {
+      res.json({})
+    })
+})
+
+postRouter.put('/:postId', (req, res) => {
+  postApi.updatePost(req.params.postId, req.body)
+    .then(() => {
+      res.json({})
+    })
+})
+
+postRouter.delete('/:postId', (req, res) => {
+  postApi.deletePost(req.params.postId)
+    .then(() => {
+      res.json({})
+    })
+})
+
 module.exports = {
  postRouter
 }
