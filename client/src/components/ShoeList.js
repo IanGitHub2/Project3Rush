db61f9c7efc7b2a770c2c8ba4bcdbb0d3e7f6bfa
Ian Kincaid <ikbusinessacct@gmail.com>
2020-01-23 00:34:47 -0500

    shoe list done

---
+++ client/src/components/ShoeList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default class ShoeList extends React.Component{
+    render(){
+        return(
+            <div>
+                {this.props.ShoeList.map((shoe, i) => {
+                    return(
+                        <div>
+                            <div>
+                                <Link to={`/single-shoe/${shoe._id}`}>
+                                    <img src={shoe.image} alt="shoe" width='400' height='200'/>
+                                </Link>
+                            </div>
+                            <div>
+                                <div>{shoe.brand}</div>
+                                <div>{shoe.name}</div>
+                                <div>{shoe.size}</div>
+                                <div>{shoe.release}</div>
+                            </div>
+                            <div>
+                                <div>{shoe.description}</div>
+                            </div>
+                        </div>
+                    )
+                })}
+            </div>
+        )
+    }
+}
